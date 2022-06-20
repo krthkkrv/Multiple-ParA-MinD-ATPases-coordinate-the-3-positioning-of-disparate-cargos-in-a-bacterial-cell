27929b58417ebc7d5227ef3d045c42661c46073b
Keerthikka Ravi <48771728+krthkkrv@users.noreply.github.com>
2022-06-20 16:13:10 -0400

    Update Rscript.r

--- R/Rscript.r
+++ R/Rscript.r
@@ -17,31 +17,31 @@ library("extraoperators")
 # merge_overlapping_ranges_dataset()
 # condensed_phylum()
 
-## Running the blast inputs
-#  list of phylum names
+## Running the tblastn inputs
+#  List of phylum names
 phylum_list <- c( "Acidobacteria","Actinobacteria","Alphaproteobacteria", "Aquificae", "Bacteroidetes", "Betaproteobacteria", "Chlamydiae", "Chlorobi",'Chloroflexi','Cyanobacteria','Deinococcales', 'Deltaproteobacteria', 'Epsilonproteobacteria', 'Fibrobacteres','Firmicutes', 'Fusobacteria', 'Gammaproteobacteria', 'Planctomycetes', "Spirochaetes", 'Thermotogae', 'Zetaproteobacteria')
 
-#  list of query proteins
+#  List of query proteins
 protein_list <- c("ParA", "ParC", "McdA", "FlhG", "MinD")
 
-#  the main path to the subfolders holding the blast output downloads
-path <- "../Consensus_based_tBLASTn/"
+#  The main path to the subfolders holding the tblastn output downloads
+path_to <- "../Consensus_based_tBLASTn/"
 
 #  Creating a empty list
 Consensus_tBLASTn <- NULL
 
-#  combining the blast output in a phylum specific manner and adding to the empty list
+#  Combining the blast output in a phylum specific manner and adding to the empty list
 for(i in 13:length(phylum_list)){
   phylum <- phylum_list[i]
   print(phylum)
   names(Consensus_tBLASTn)[[i]] <- phulym
-  Consensus_tBLASTn[[i]] <- as.data.frame(condensed_phylum(phylum = phylum, path = "Consensus_based_tBLASTn/", protien = protein_list))
+  Consensus_tBLASTn[[i]] <- as.data.frame(condensed_phylum(phylum = phylum, path = path_to, protein = protein_list))
 }
 
 ## Generating the hits_ratio_table
-#  creating an empty list
+#  Creating an empty list
 binary_table <- NULL
-#  looping thorough the listss of data frame to generate one final data frame with columns specifying the number of parA hits per species, the hits in heat genetic element, the ration of hits/genetic_element, as well as a count of different ParA-like protein hits found in a species.
+#  Looping thorough the lists of data frame to generate one final data frame with columns specifying the number of ParA/MinD hits per species, the hits in each genetic element, the number of unique ParA/MinD hits in each species, the ratio of hits/genetic_element, as well as a count of different ParA/MinD-like protein hits found in a species.
 
 for(i in 1:length(Consensus_tBLASTn)){
   df <- as.data.frame(Consensus_tBLASTn[[i]])
@@ -79,7 +79,7 @@ for(i in 1:length(Consensus_tBLASTn)){
     # nmbr.genetic.elements
     nmbr_genetic_elements <- append(nmbr_genetic_elements, length(unique(sub$name2)))
 
-    #plasmid/chromosome
+    # plasmid/chromosome
     sub_1 <- subset(sub, sub[,"genome_type"] == "plasmid")
     plasmid <-  append(plasmid,length(unique(sub_1$name2)))
     sub_1 <- subset(sub, sub[,"genome_type"] == "chromosome")
@@ -93,7 +93,7 @@ for(i in 1:length(Consensus_tBLASTn)){
     minD <- append(minD, sum(name1 == "MinD"))
     flhG <- append(flhG, sum(name1 == "FlhG"))
     
-    #unknown.cargo.ParAs
+    # unknown.cargo.ParAs
     n=0
     for(i in 1:length(name1)){
       if(nchar(name1[i])>4){
@@ -101,7 +101,7 @@ for(i in 1:length(Consensus_tBLASTn)){
     }
     other_parAs <- append(other_parAs, n)
   }
-  #knitting all the lists
+  # knitting all the lists
   output <- data.frame("Scientific.Name" = sp.Name,
                        "chromosome.accession" = g.acc,
                        "total.ParA.hits" = as.numeric(sp.hit.count),
@@ -123,10 +123,10 @@ for(i in 1:length(Consensus_tBLASTn)){
   binary_table <- rbind(binary_table, output)
 }
 
-#  calculating the hits/genetic_element ratio
+# Calculating the hits/genetic_element ratio
 binary_table$ratio <- binary_table$total.ParA.hits/binary_table$nmbr.genetic.elements
 
-# cleaning the scientifc.Name column so that they match the naming format in the AssemeblyAccession.csv file
+# Cleaning the Scientifc.Name column so that they match the naming format in the AssemeblyAccession.csv file
 tmp <- binary_table %>% 
     separate(Scientific.Name, c("name1", "name2"), extra = "drop", fill = "right", remove = T)
   
@@ -134,29 +134,29 @@ tmp1 <- na.omit(tmp)
 tmp1$Scientific.Name <- paste(tmp1$name1, tmp1$name2, sep = " ")
 binary_table <- tmp1[,c(17,3,4,5,6,7,9,8,16,10:15)]
 
-# Geting unique ParA hits for each species
+# Getting unique ParA/MinD hits for each species
 binar_table$unique.ParAs -> binary_table$total.ParA.hits - binary_table$nmbr.genetic.elements + 1
 
-# reading in Assembly accession file 
+# Reading in Assembly accession file 
 AssemblyAccession <- read.csv("../AssemblyAccession.csv")
 
-# merging in the Assembly accession numbers
+# Merging in the Assembly accession numbers
 binary_table_meta <- merge(AssemblyAccession, binary_table, by = "Scientific.Name", all.y = T)
 
 ## Adding Taxonomy information to the data
-# getting taxid
+# Getting taxid
 query <- sort(binary_table$chromosome.accession)[9:11905]
 quer_op <- esummary_fetch_taxid(query)
 colnames(quer_op)[1] <- "chromosome.accession"
 
-# adding the taxID information to the data (needed for taxonomy information)
+# Adding the taxID information to the data (needed for taxonomy information)
 binary_table_meta <- merge(quer_op, binary_table, by = "chromosome.accession", all.y = T)
 
-# reading in the taxonomy file (has info for phylum,class....)
+# Reading in the taxonomy file (has info for phylum,class....)
 taxonomy <- read_xlsx("../Taxonomy_info.xlsx") %>% 
   as.data.frame()
 
-# final metadata output
+# Final metadata output
 binary_table_meta <- merge(taxonomy[,c(1,3,4)], binary_table_meta, by = "taxId", all.y = T)
 
 --complete--
